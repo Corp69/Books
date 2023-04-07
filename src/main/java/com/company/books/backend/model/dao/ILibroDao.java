7e28d72e7a8b8cfcfc52ff7f78bed0150221c154
Corpsey <102923473+Corp69@users.noreply.github.com>
2023-04-07 16:35:05 -0600

    Terminamos un CRDU completo de mi api

--- src/main/java/com/company/books/backend/model/dao/ILibroDao.java
+++
@@ -1,9 +0,0 @@
-package com.company.books.backend.model.dao;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.company.books.backend.model.Libro;
-
-public interface ILibroDao extends CrudRepository<Libro, Long>{
-
-}
